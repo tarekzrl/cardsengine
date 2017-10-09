9be00179e5e899a8e1122150d14e0b78532998e0
tarekzrl <31318544+tarekzrl@users.noreply.github.com>
2017-10-09 08:34:51 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -17,7 +17,7 @@ $(function () {
 
         if (q != "undefined" && q != "") {
                 $("#search_box").attr("value",q);
-                var uri = "https://dcmongo.herokuapp.com/mongo?q="  + qGet;
+                var uri = "http://localhost:3000/mongo?q="  + qGet;
                 sendAjax(uri);
         }
 
@@ -36,104 +36,81 @@ $(function () {
         });
 
 });
-
+var resultItem ="";
+		
 function sendAjax(uri) {
         $.ajax({
                	url: uri,
                 dataType: "json",
                 success: function(data) {
-//		var json = $.parseJSON(data);
 		$.each(data, function(i,data) {
-		var resultItem;
+		
                                                 if (data.player)
                                                 {
 							if(data.player.substring(data.player.lastIndexOf('.') + 1) =="mp4") {
-	                                                resultItem= "<div class='item'><video width='100%' height='100%' controls><source src='"+data.player+"'></source></video></div>";
-                             	$("#results-block").append(resultItem);}
+	                                                resultItem += "<div class='item'><video width='100%' height='100%' controls><source src='"+data.player+"'></source></video></div>";
+                             	}
 							else if(data.site=="Instagram") {
-	                                                resultItem= "<div class='item'>"+data.oembed.html+"</div>";
-                             	$("#results-block").append(resultItem);}
+	                                                resultItem += "<div class='item'>"+data.oembed.html+"</div>";
+                             	}
 							else
-{                                                	resultItem= "<div class='player'><iframe style='position:absolute; border: 0; width: 100%; height: 100%;' allowfullscreen src='"+data.player+"'></iframe></div>";
-                             	$("#results-block").append(resultItem);}
+{                                                	resultItem += "<div class='player'><iframe style='position:absolute; border: 0; width: 100%; height: 100%;' allowfullscreen src='"+data.player+"'></iframe></div>";
+                             	}
 						}
                                                 else if (data.html)
                                                 {
-                                                                resultItem= "<div class='item'>"+data.html+"</div>";
-                             	$("#results-block").append(resultItem);
+                                                                resultItem += "<div class='item'>"+data.html+"</div>";
+                             	
                                                 }
                                                 else if (data.oembed)
-	                                        {        if (data.oembed.html)
+	                                        {
+						        if ((data.oembed.html) && (data.oembed.html.indexOf("<")== 0))
         	                                        {
                                                                 if ((data.oembed.html).substring(10, 26)=="twitter-timeline")
                                                                 {
-                                                                resultItem= "<div class='item'><a class='twitter-timeline' data-tweet-limit=1 href='"+data.canonical+"'></a><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script></div>";
-				                             	$("#results-block").append(resultItem);
-                                                                }
+                                                                resultItem += "<div class='item'><a class='twitter-timeline' data-tweet-limit=1 href='"+data.canonical+"'></a><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script></div>";
+								}
+                                                                else if ((data.site)=="www.facebook.com")
+                                                                {
+                                                                resultItem += "<div class='item'><div id=\'fb-root\'></div>\n<script>(function(d, s, id) {\n  var js, fjs = d.getElementsByTagName(s)[0];\n  if (d.getElementById(id)) return;\n  js = d.createElement(s); js.id = id;\n  js.src = \'//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10\';\n  fjs.parentNode.insertBefore(js, fjs);\n}(document, 'script', 'facebook-jssdk'));</script><div class=\'fb-page\' data-href=\'"+data.canonical+"\' data-small-header=\'\' data-adapt-container-width=\'1\' data-hide-cover=\'\' data-width=\'500\' data-show-facepile=\'1\'><blockquote cite=\'"+data.canonical+"\' class=\'fb-xfbml-parse-ignore\'><a href=\'"+data.canonical+"\'>"+data.title+"</a></blockquote></div></div>";
+								}
                                                                 else if (data.oembed.type=='video')
 								{
-                                                                resultItem= "<div class='player'>"+data.oembed.html+"</div>";
-                             					$("#results-block").append(resultItem);
+                                                                resultItem += "<div class='player'>"+data.oembed.html+"</div>";
 								}
-                                                                else if ((data.oembed.provider_name=='Variety') || (data.oembed.provider_name=='TechCrunch'))
+                                                                else if ((data.oembed.provider_name=='Variety') || (data.oembed.provider_name=='WIRED') || (data.oembed.provider_name=='TechCrunch'))
 								{
-                                                		resultItem= "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
-                             					$("#results-block").append(resultItem);
+                                                		resultItem += "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
 								}
                                                                 else
 								{		
-                                                                resultItem= "<div class='item'>"+data.oembed.html+"</div>";
-				                             	$("#results-block").append(resultItem);
+                                                                resultItem += "<div class='item'>"+data.oembed.html+"</div>";
+				                             	
 								}
                 	                                }
-	                                                else if (data.image)
+	                                                else if ((data.image) && (data.width > 399))
         	                                        {
-
-                	                                        var imag=new Image();
-                        	                                imag.src=data.image;
-                                	                        $(imag).load(function() {
-                                        	                        if(imag.width>399)
-                                                	                {
-									resultItem= "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
-					                                $("#results-block").append(resultItem);
-                                        	                        }
-                                                	        });
-                                                	}
-        	                                        else if (data.oembed.image)
+														resultItem += "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
+														
+													}
+        	                                        else if ((data.oembed.image) && (data.oembed.width > 399))
         	                                        {
-	
-                	                                        var imag=new Image();
-                        	                                imag.src=data.oembed.image;
-                                	                        $(imag).load(function() {
-                                        	                        if(imag.width>399)
-                                                	                {
-									resultItem= "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.oembed.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
-					                                $("#results-block").append(resultItem);
-                                        	                        }
-                                                	        });
-	                                                	}
+														resultItem += "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.oembed.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
+														
+													}
 
 							}
-                                                else if (data.image)
+                                                else if ((data.image) && (data.width > 399))
                                                 {
-
-                                                        var imag=new Image();
-                                                        imag.src=data.image;
-                                                        $(imag).load(function() {
-                                                                if(imag.width>399)
-                                                                {
-				resultItem= "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
-                             	$("#results-block").append(resultItem);
-								}
-                         });
-                                                }
-						else
-						{
-						var text = JSON.stringify(data,null, "  ");
-						var resultJson = "<xmp>" + text + "</xmp>";
-		                             	$("#results-block").append(resultJson);
-						}
+													resultItem += "<div class='item'><div class='article'><a class='l' title='"+data.title+"' href='"+data.canonical+"' style='background-image: url("+data.image+");'></a><div class='o'><header class='h'><h1 class='t' title='"+data.title+"'>"+data.title+"</h1><p class='m'><img style='width:16px;height:16px;margin-right:.125em;vertical-align:middle;' src='"+data.icon+"'><a href='"+data.canonical+"' target='_blank'>"+data.site+"</a></p></header></div></div></div>";
+													
+												}
+						
                	});
-        }
+        },
+
+		complete: function() {
+				$("#results-block").append(resultItem);
+			}
 });
 }
